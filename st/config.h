8c860adce1437fc930f28003ea20a0851d19f068
Alejandro Bernal <jbernal@unal.edu.co>
2022-12-11 19:21:34 -0500

    added line height

--- st/config.h
+++ st/config.h
@@ -51,7 +51,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.0;
+static float chscale = 1.3; // line height, default 1.0
 
 /*
  * word delimiter string
